9153716add8f71afdaeec86e51e236c7ea0d23b6
Schlickmann <nany.julychi@gmail.com>
2018-04-21 21:21:57 -0300

    Start

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Juliani</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
